2c33de41b625a8cf5d7593c89a6132b00501b2bf
mersung <86938974+mersung@users.noreply.github.com>
2024-09-20 18:33:15 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -32,10 +32,39 @@
 # )
 # order by r.review_date, review_text
 
-select p.member_name, r.review_text, DATE_FORMAT(r.review_date, '%Y-%m-%d') as rDate from MEMBER_PROFILE p INNER JOIN REST_REVIEW r
-on p.member_id = r.member_id
-where p.member_id = (select member_id from REST_REVIEW 
-group by member_id
-order by COUNT(REVIEW_ID) desc
-limit 1)
-order by rDate, r.review_text
+# select p.member_name, r.review_text, DATE_FORMAT(r.review_date, '%Y-%m-%d') as rDate from MEMBER_PROFILE p INNER JOIN REST_REVIEW r
+# on p.member_id = r.member_id
+# where p.member_id = (select member_id from REST_REVIEW 
+# group by member_id
+# order by COUNT(REVIEW_ID) desc
+# limit 1)
+# order by rDate, r.review_text
+
+# select MEMBER_NAME, REVIEW_TEXT, date_format(REVIEW_DATE, '%Y-%m-%d') review_date from member_profile
+# inner join rest_review on member_profile.member_id = rest_review.member_id
+# where member_profile.member_id = (
+#     select member_id from rest_review
+#     group by member_id
+#     order by count(*) desc
+#     limit 1
+# )
+# order by review_date, review_text
+
+
+SELECT MEMBER_NAME, REVIEW_TEXT, DATE_FORMAT(REVIEW_DATE, '%Y-%m-%d') FROM MEMBER_PROFILE P INNER JOIN
+REST_REVIEW R ON P.MEMBER_ID = R.MEMBER_ID
+WHERE P.MEMBER_ID = (
+    SELECT MEMBER_ID FROM REST_REVIEW
+    GROUP BY MEMBER_ID
+    ORDER BY COUNT(*) DESC
+    LIMIT 1
+)
+ORDER BY REVIEW_DATE, REVIEW_TEXT
+
+
+
+
+
+
+
+
