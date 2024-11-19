25b1d519077a9fc4f74f9c327dd203f20e43a25e
mersung <86938974+mersung@users.noreply.github.com>
2024-11-19 22:58:42 +0900

    [level 2] Title: 자동차 평균 대여 기간 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/157342. 자동차 평균 대여 기간 구하기/자동차 평균 대여 기간 구하기.sql
+++ 프로그래머스/2/157342. 자동차 평균 대여 기간 구하기/자동차 평균 대여 기간 구하기.sql
@@ -5,10 +5,28 @@
 # order by average_duration desc, car_id desc
 
 
-select car_id, ROUND(AVG(DATEDIFF(END_DATE,START_DATE)+1), 1) as AVERAGE_DURATION
+# select car_id, ROUND(AVG(DATEDIFF(END_DATE,START_DATE)+1), 1) as AVERAGE_DURATION
+# from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+# group by car_ID
+# having(average_duration) >= 7
+# order by average_duration desc, car_id desc
+
+# SELECT CAR_ID, ROUND(AVG(DATEDIFF(END_DATE, START_DATE)+1), 1) AVERAGE_DURATION 
+# FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+# GROUP BY CAR_ID
+# HAVING AVERAGE_DURATION >= 7
+# ORDER BY AVERAGE_DURATION DESC, CAR_ID DESC;
+
+# select car_id, ROUND(AVG(DATEDIFF(END_DATE, START_DATE) + 1), 1) AVERAGE_DURATION
+# from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+# group by car_id
+# having AVERAGE_DURATION >= 7
+# order by AVERAGE_DURATION DESC, CAR_ID DESC
+
+select car_id, ROUND(avg(DATEDIFF(end_date, start_date))+1, 1) average_duration
 from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-group by car_ID
-having(average_duration) >= 7
+group by car_id
+having average_duration >= 7
 order by average_duration desc, car_id desc
 
 
